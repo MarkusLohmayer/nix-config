970bbec497aec1a75aeda6fda67cbdb351363b57
Markus Lohmayer <markus.lohmayer@gmail.com>
2023-06-30 16:19:48 +0200

    nixvim added leap

--- home/nixvim/leap.nix
+++ home/nixvim/leap.nix
@@ -1,10 +1,7 @@
-{pkgs, ...}: {
+{...}: {
   config = {
-    extraPlugins = with pkgs.vimPlugins; [
-      leap-nvim
-    ];
-    extraConfigLua = ''
-      require("leap").add_default_mappings()
-    '';
+    plugins.leap = {
+      enable = true;
+    };
   };
 }
