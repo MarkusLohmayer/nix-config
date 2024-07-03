dfddfb7df6a715cd9f4509642d1fdc9254e14580
Markus Lohmayer <markus.lohmayer@gmail.com>
2024-07-03 18:22:10 +0200

    revise home/ssh

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,22 +1,30 @@
-{...}: {
+{config, ...}: {
   programs.ssh = {
     enable = true;
+    addKeysToAgent = "yes";
+    forwardAgent = true;
+    hashKnownHosts = false;
     serverAliveInterval = 30;
     serverAliveCountMax = 6;
+    matchBlocks = {
+      "github.com" = {
+        identityFile = "${config.home.homeDirectory}/.ssh/id_rsa";
+      };
+      "git.ltd.uni-erlangen.de" = {
+        identityFile = "${config.home.homeDirectory}/.ssh/id_rsa";
+        port = 22;
+        user = "git";
+      };
+      "ltd.uni-erlangen.de" = {
+        identityFile = "${config.home.homeDirectory}/.ssh/id_rsa";
+        match = "Host *.ltd.uni-erlangen.de";
+        port = 8822;
+        user = "di29nume";
+      };
+    };
     extraConfig = ''
-      Host *
-        IdentityFile ~/.ssh/id_rsa
-        IgnoreUnknown UseKeychain
-        UseKeychain yes
-        AddKeysToAgent yes
-
-      Host git.ltd.uni-erlangen.de
-        Port 22
-        User git
-
-      Match Host *.ltd.uni-erlangen.de
-        Port 8822
-        User di29nume
+      IgnoreUnknown UseKeychain
+      UseKeychain yes
     '';
   };
 }
